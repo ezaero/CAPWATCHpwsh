2bcce29e76d22fb29c09dd48475c6607e1260afb
Michael Schulte <mike.schulte@ezaero.com>
2025-03-09 18:34:54 -0600

    added update-teams

--- list-distribution-lists/run.ps1
+++ list-distribution-lists/run.ps1
@@ -14,7 +14,9 @@ $CAPWATCHDATADIR = "$($env:HOME)\data\CAPWatch"
 Push-Location $CAPWATCHDATADIR
 
 #Abort script execution if CAPWATCH data is stale
-if (((Get-Date) - ((Import-Csv .\DownLoadDate.txt -ErrorAction Stop).DownLoadDate | Get-Date)).TotalHours -gt 24) {
+$DownloadDate = (((Get-Date) - ((Import-Csv .\DownLoadDate.txt -ErrorAction Stop).DownLoadDate | Get-Date)).TotalHours)
+Write-Host "Download date is: [$DownloadDate]"
+if (((Get-Date) - ((Import-Csv .\DownLoadDate.txt -ErrorAction Stop).DownLoadDate | Get-Date)).TotalHours -gt 48) {
     Write-Error "CAPWATCH data in [$CAPWATCHDATADIR] is stale; aborting script execution!"
     exit 1
 }
@@ -30,39 +32,50 @@ $allDistLists = Get-DistributionGroup -ResultSize Unlimited
 Write-Host "Total DLs" $allDistLists.count
 
 $allDistLists | ForEach-Object {
-    Write-Progress -activity "Processing $_.DisplayName" -status "$i out of $totalmbx completed"
+    Write-Host "Distribution List: $($_.DisplayName)"
+    $members = Get-DistributionGroupMember -Identity $_.Identity
+    Write-Host "Members:"
+    $members | ForEach-Object {
+        # if ($_.RecipientType -eq "UserMailbox" -or $_.RecipientType -eq "MailUser") {
+        #     $user = Get-User -Identity $_.Identity
+        #     Write-Host $user.DisplayName
+        # } else {
+            Write-Host $_.Name
+        # }
+    }
+    Write-Host "-----------------------------"
 }
 
-$allMgUsers = Get-MgUser -All
+# $allMgUsers = Get-MgUser -All
 
-$MatchedUsers = $allMgUsers.where({ $_.OfficeLocation -in $CAPWATCH_Member.CAPID })
+# $MatchedUsers = $allMgUsers.where({ $_.OfficeLocation -in $CAPWATCH_Member.CAPID })
 
-foreach ($MatchedUser in $MatchedUsers) {
-    $_capwatchMatchedUser = $CAPWATCH_Member.where({ $_.CAPID -eq $MatchedUser.OfficeLocation })
-    if ($_capwatchMatchedUser.count -gt 1) {
-        Write-Error "Multiple members found in CAPWATCH data for $($MatchedUser.UserPrincipalName) $($MatchedUser.OfficeLocation) - skipping"
-        continue
-    }
+# foreach ($MatchedUser in $MatchedUsers) {
+#     $_capwatchMatchedUser = $CAPWATCH_Member.where({ $_.CAPID -eq $MatchedUser.OfficeLocation })
+#     if ($_capwatchMatchedUser.count -gt 1) {
+#         Write-Error "Multiple members found in CAPWATCH data for $($MatchedUser.UserPrincipalName) $($MatchedUser.OfficeLocation) - skipping"
+#         continue
+#     }
 
-    #Build the splat object for the Update-MgUser cmdlet
-    $_updateObject = @{
-        UserId      = ($MatchedUser.UserPrincipalName)
-        DisplayName = ('{0} {1}, {2}' -f $_capwatchMatchedUser.NameFirst.Trim(), $_capwatchMatchedUser.NameLast.Trim(), $_capwatchMatchedUser.Rank.Trim())
-        JobTitle    = ($_capwatchMatchedUser.Rank.Trim())
-    }
+#     #Build the splat object for the Update-MgUser cmdlet
+#     $_updateObject = @{
+#         UserId      = ($MatchedUser.UserPrincipalName)
+#         DisplayName = ('{0} {1}, {2}' -f $_capwatchMatchedUser.NameFirst.Trim(), $_capwatchMatchedUser.NameLast.Trim(), $_capwatchMatchedUser.Rank.Trim())
+#         JobTitle    = ($_capwatchMatchedUser.Rank.Trim())
+#     }
 
-    #Test if the user needs an update
-    $_NeedsUpdate = @(
-        ($MatchedUser.DisplayName -ne $_updateObject.DisplayName)
-        ($MatchedUser.JobTitle -ne $_updateObject.JobTitle)
-    ) -contains $true
+#     #Test if the user needs an update
+#     $_NeedsUpdate = @(
+#         ($MatchedUser.DisplayName -ne $_updateObject.DisplayName)
+#         ($MatchedUser.JobTitle -ne $_updateObject.JobTitle)
+#     ) -contains $true
 
-    #Perform updates on users that are out-of-date
-    if ($_NeedsUpdate) {
-        Write-Host "Updating user '$($_updateObject.UserId)' with DisplayName: '$($_updateObject.DisplayName)' and JobTitle '$($_updateObject.JobTitle)'"
-        Update-MgUser @_updateObject
-    }
-    else {
-        Write-Host "Skipping user $($_updateObject.UserId) - already up to date"
-    }
-}
+#     #Perform updates on users that are out-of-date
+#     if ($_NeedsUpdate) {
+#         Write-Host "Updating user '$($_updateObject.UserId)' with DisplayName: '$($_updateObject.DisplayName)' and JobTitle '$($_updateObject.JobTitle)'"
+#         Update-MgUser @_updateObject
+#     }
+#     else {
+#         Write-Host "Skipping user $($_updateObject.UserId) - already up to date"
+#     }
+# }
---
+++ updateTeams/run.ps1
@@ -0,0 +1,161 @@
+# Input bindings are passed in via param block.
+param($Timer)
+
+#region ImportClasses
+<# 
+    Using strongly typed classes allows us to both transform data
+    (like dates) from [STRING] to their appropriate object types (like [DATETIME])
+#>
+. "$PSScriptRoot\classes\Member.ps1"
+#endregion
+
+# Set working directory to folder with all CAPWATCH CSV Text Files
+$CAPWATCHDATADIR = "$($env:HOME)\data\CAPWatch"
+Push-Location $CAPWATCHDATADIR
+
+#Abort script execution if CAPWATCH data is stale
+if (((Get-Date) - ((Import-Csv .\DownLoadDate.txt -ErrorAction Stop).DownLoadDate | Get-Date)).TotalHours -gt 40) {
+    Write-Error "CAPWATCH data in [$CAPWATCHDATADIR] is stale; aborting script execution!"
+    exit 1
+}
+
+# Define the path to the DutyPosition.txt file
+# Read the DutyPosition.txt file and reduce it to only WING Staff
+$dutyPositions_all = Import-Csv .\DutyPosition.txt -ErrorAction Stop
+$dutyPositions = $dutyPositions_all | Where-Object { $_.Lvl -eq "WING" }
+$dutyPositions = $dutyPositions | Sort-Object CAPID -Unique
+
+# Connect to Microsoft Graph
+Connect-MgGraph -Scopes "User.Read.All","TeamMember.ReadWrite.All"
+
+# This function takes 2 arrays and compares them, return 3 Arrays
+function Compare-UserIds {
+    param (
+        [array]$Array1,
+        [array]$Array2
+    )
+
+    # Find user IDs that are in both arrays
+    $inBoth = $Array1 | Where-Object { $Array2 -contains $_ }
+
+    # Find user IDs that are only in Array1
+    $AddtoTeams = $Array1 | Where-Object { $Array2 -notcontains $_ }
+
+    # Find user IDs that are only in Array2
+    $RemovefromTeams = $Array2 | Where-Object { $Array1 -notcontains $_ }
+
+    # Output the results
+    [PSCustomObject]@{
+        InBoth       = $inBoth
+        AddtoTeams = $AddtoTeams
+        RemovefromTeams = $RemovefromTeams
+    }
+}
+
+# Takes in an Array of userId's and prints the display names of each of them
+function Get-DisplayNames {
+    param (
+        [array]$UserIds
+    )
+
+    foreach ($userId in $UserIds) {
+        try {
+            # Get the user details
+            $user = Get-MgUser -UserId $userId
+
+            # Extract and print the displayName
+            $displayName = $user.displayName
+            Write-Output "User ID: $userId, Display Name: $displayName"
+        } catch {
+            Write-Output "Failed to retrieve display name for User ID: $userId"
+        }
+    }
+}
+
+# Fetch all users from Microsoft Entra ID (Azure AD)
+$allUsers = @()
+$uri = "https://graph.microsoft.com/v1.0/users?$select=id,displayName,officeLocation"
+do {
+    $response = Invoke-MgGraphRequest -Method GET -Uri $uri
+    $allUsers += $response.value
+    $uri = $response.'@odata.nextLink'
+} while ($uri)
+
+# Initialize the WINGSTAFF array
+$wingStaffUsers = @()
+
+# Create a hash table to store users by CAPID
+$userHashTable = @{}
+foreach ($user in $allUsers) {
+    if ($user.officeLocation) {
+        $userHashTable[$user.officeLocation] = $user.id
+    }
+}
+
+# Process each entry in the DutyPosition file
+foreach ($position in $dutyPositions) {
+    $CAPID = $position.CAPID
+    if ($userHashTable.ContainsKey($CAPID)) {
+#        $EmployeeID = $userHashTable[$CAPID]
+        if ($position.Lvl -eq "WING") {
+#            $DEPARTMENT = "$($position.Lvl) $($position.Duty)"
+            # Add the CAPID to the WINGSTAFF array
+            $wingStaffUsers += $allUsers | Where-Object { $_.officeLocation -eq $CAPID }
+        }
+    }
+}
+
+# Define the Microsoft Team ID (Wing Staff)
+$teamId = "4ea99e49-088a-4996-8dd5-966b6ca408c7"
+
+# Get the current members of the team
+$teamMembers = @()
+$uri = "https://graph.microsoft.com/v1.0/teams/$teamId/members"
+do {
+    $response = Invoke-MgGraphRequest -Method GET -Uri $uri
+    $teamMembers += $response.value
+    $uri = $response.'@odata.nextLink'
+} while ($uri)
+
+
+# Create an array to store current team members by userId
+$currentTeamMemberIds = @()
+foreach ($member in $teamMembers) {
+    # $user = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/beta/users/$($member.userId)?$select=officeLocation"
+    # if ($user.officeLocation) {
+        $currentTeamMemberIds += $member.userId
+    # }
+}
+
+# Extract the user IDs
+$wingUserIds = $wingStaffUsers | Select-Object -ExpandProperty id
+
+$result = Compare-UserIds -Array1 $wingUserIds -Array2 $currentTeamMemberIds
+# Display the results
+Write-Output "User IDs in both arrays:"
+Get-DisplayNames -UserIds $result.InBoth
+
+Write-Output "User IDs that should be added to the Team:"
+Get-DisplayNames -UserIds $result.AddtoTeams
+
+Write-Output "User IDs in Teams that should be removed:"
+Get-DisplayNames -UserIds $result.RemovefromTeams
+
+# Add all new Staff members to the team
+foreach ($userId in $result.AddtoTeams) {
+    $user = Get-MgUser -Filter "id eq '$userId'"
+    $body = @{
+        "@odata.type" = "#microsoft.graph.aadUserConversationMember"
+        "roles" = @()
+        "user@odata.bind" = "https://graph.microsoft.com/v1.0/users/$($userId)"
+    }
+    Invoke-MgGraphRequest -Method POST -Uri "https://graph.microsoft.com/v1.0/teams/$teamId/members" -Body ($body | ConvertTo-Json -Depth 10)
+    Write-Output "Added user: $userId to the team"
+} 
+
+# Remove any members from the team who are not in WINGSTAFF
+foreach ($userId in $result.RemovefromTeams) {
+    $membershipId = $teamMembers | Where-Object { $_.userId -eq $userId } | Select-Object -ExpandProperty id
+    Invoke-MgGraphRequest -Method DELETE -Uri "https://graph.microsoft.com/v1.0/teams/$teamId/members/$membershipId"
+    Write-Output "Removed CAPID: $userId from the team"
+}
